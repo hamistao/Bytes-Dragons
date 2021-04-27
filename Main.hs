83ed029a64699f3e31d31820b4d6c31618efb536
Pedro Ribeiro <pedro.antonio.ribeiro@ccc.ufcg.edu.br>
2021-04-27 18:52:07 -0300

    atualizando main na minha branch pra dar merge

--- Main.hs
+++ Main.hs
@@ -134,7 +134,101 @@ listarItensNomes = do
             print "---> "
             putStrLn $ contents
             print " <---"
+<<<<<<< Updated upstream
             restart main
+=======
+            hClose handle
+            restart menuItem
+        else do
+            createDirectoryIfMissing True $ takeDirectory tipo
+            writeFile "data/equip.info" ""
+            writeFile "data/consmvl.info" ""
+            listarItensNomes tipo
+
+
+cadastrarItem :: String -> IO ()
+cadastrarItem tipo = do
+    existsEquip <- doesFileExist "data/equip.info"
+    existsConsmvl <- doesFileExist "data/consmvl.info"
+    if existsEquip && existsConsmvl
+        then do
+            criarItemTipo tipo
+        else do
+            createDirectoryIfMissing True $ takeDirectory tipo
+            writeFile "data/equip.info" ""
+            writeFile "data/consmvl.info" ""
+            cadastrarItem tipo
+
+
+criarItemTipo :: String -> IO ()
+criarItemTipo tipo
+    | tipo == "data/equip.info" = criarItemEquipavel tipo
+    | tipo == "data/consmvl.info" = criarItemConsmvl tipo
+    | otherwise = putStrLn "Erro na Abertura de arquivo\n"
+
+
+criarItemEquipavel :: String -> IO ()
+criarItemEquipavel path = do
+    putStrLn "Qual o nome do Equipavel?"
+    nome <- getLine
+    putStrLn "Qual a Alteração de Resistência?"
+    resistnc <- getLine
+    putStrLn "Qual a Alteração de Velocidade?"
+    velocd <- getLine
+    putStrLn "Onde sera Equipavel (Torso, Cabeca, Pernas, Maos)"
+    tipo <- getLine
+    appendFile path (show (Item.criaEquipavel nome (read resistnc) (read velocd) (read tipo :: Item.TipoEquipavel)) ++ "\n")
+    putStrLn "Item Criado"
+    restart menuEquip
+
+
+criarItemConsmvl :: String -> IO ()
+criarItemConsmvl path = do
+    putStrLn "Qual o nome do Consumivel?"
+    nome <- getLine
+    putStrLn "Qual a Alteração de Vida?"
+    vida <- getLine
+    putStrLn "Qual a Alteração de Resistência?"
+    resistnc <- getLine
+    putStrLn "Qual a Alteração de Dano?"
+    dano <- getLine
+    putStrLn "Qual a Durabilidade?"
+    durac <- getLine
+    appendFile path (show (Item.criaConsumivel nome (read vida) (read resistnc) (read dano) (read durac)) ++ "\n")
+    putStrLn "Item Criado"
+    restart menuConsumvl
+
+
+getItens :: String -> String -> String
+getItens tipo contents
+    | tipo == "data/equip.info" = Item.listarEquipaveis (transformaListaEquipavel (lines contents))
+    | tipo == "data/consmvl.info" = Item.listarConsumiveis (transformaListaConsumivel (lines contents))
+    | otherwise = "Erro na leitura de Arquivo\n"
+
+
+transformaListaEquipavel :: [String] -> [Equipavel]
+transformaListaEquipavel [] = []
+transformaListaEquipavel (x:xs) = ((read :: String -> Equipavel) x) : (transformaListaEquipavel xs)
+
+
+transformaListaConsumivel :: [String] -> [Consumivel]
+transformaListaConsumivel [] = []
+transformaListaConsumivel (x:xs) = (read x :: Consumivel) : (transformaListaConsumivel xs)
+
+
+excluiItem :: String -> IO ()
+excluiItem tipo = do
+    system "clear"
+    existsEquip <- doesFileExist "data/equip.info"
+    existsConsmvl <- doesFileExist "data/consmvl.info"
+    if existsEquip && existsConsmvl
+        then do
+            handle <- openFile tipo ReadMode
+            contents <- hGetContents handle
+            getArquivoExcluir tipo contents
+            hClose handle
+            restart menuItem
+>>>>>>> Stashed changes
         else do
             putStrLn "Não ha itens criados\nNecessário criar pelo menos um Item\nEnter parar voltar ao Menu"
             restart main
