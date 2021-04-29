904cab608e030c52afb7870c4411cac10c71c890
MelquiSilva <48794046+MelquiSilva@users.noreply.github.com>
2021-04-29 14:45:50 -0300

    Aplicando imunidade a ataques na batalha
    Agora o personagem resiste se tiver a imunidade a habilidade no ataque

--- Batalha.hs
+++ Batalha.hs
@@ -1,6 +1,7 @@
+module Batalha where
 import Personagem
 import Habilidade
-
+import Item
 selecionaAtributoRelacionado :: Atributo -> Personagem -> Int
 selecionaAtributoRelacionado Forca personagem = forca personagem
 selecionaAtributoRelacionado Inteligencia personagem = inteligencia personagem
@@ -9,32 +10,14 @@ selecionaAtributoRelacionado Destreza personagem = destreza personagem
 selecionaAtributoRelacionado Constituicao personagem = constituicao personagem
 selecionaAtributoRelacionado Carisma personagem = carisma personagem
 
-
-usaHabilidade :: Habilidade -> Personagem -> Personagem
-usaHabilidade habilidade personagem =
-    Personagem{nome_personagem = nome_personagem personagem
-        ,raca = raca personagem
-        ,classe = classe personagem
-        ,vida = vida personagem + impacto_vida habilidade
-        ,vidaMaxima = vidaMaxima personagem
-        ,forca = forca personagem
-        ,inteligencia = inteligencia personagem
-        ,sabedoria = sabedoria personagem
-        ,destreza = destreza personagem
-        ,constituicao = constituicao personagem
-        ,carisma = carisma personagem
-        ,velocidade = velocidade personagem + impacto_velocidade habilidade
-        ,ouro = ouro personagem
-        ,xp = xp personagem
-        ,xpUp = xpUp personagem
-        ,nivel = nivel personagem
-        ,equipaveis = equipaveis personagem
-        ,consumiveis = consumiveis personagem
-        ,habilidades = habilidades personagem
-    }
     
 turnoConsumivel :: Personagem -> Personagem -> Consumivel -> Personagem 
 turnoConsumivel personagemEmissor personagemReceptor consumivel = usarItemConsumivel consumivel personagemReceptor 
-turnoHabilidade :: Personagem -> Personagem -> Habilidade -> Int -> Personagem
-turnoHabilidade personagemEmissor personagemReceptor habilidade numeroDados = if (selecionaAtributoRelacionado ((atributo_relacionado habilidade) personagem) + numeroDados ) >= pontosParaAcerto habilidade then usaHabilidade habilidade personagemReceptor
-                                                                              else personagemReceptor   
+turnoHabilidade :: Personagem -> Personagem -> Habilidade -> Int -> Int-> Personagem
+turnoHabilidade personagemEmissor personagemReceptor habilidade numeroDado1 numeroDado2 = if (isImune personagemReceptor (tipoDeDano habilidade) && (selecionaAtributoRelacionado (atributo_relacionado habilidade) personagemEmissor) + (min numeroDado1 numeroDado2) >= (pontosParaAcerto (habilidade))) then usaHabilidade habilidade personagemReceptor
+                                                                                else if (selecionaAtributoRelacionado (atributo_relacionado habilidade) personagemEmissor + (numeroDado1 + numeroDado2) >= pontosParaAcerto habilidade) then usaHabilidade habilidade personagemReceptor
+                                                                                else personagemReceptor    
+
+                                                                                
+isImune :: Personagem -> TipoDano -> Bool
+isImune personagem habilidade = habilidade `elem` (imunidades personagem)
