4361aec6e418dfb603316a6174a604dc3fead6bf
MelquiSilva <48794046+MelquiSilva@users.noreply.github.com>
2021-04-29 15:30:40 -0300

    checando se personagem esta vivo ou tem imunidade

--- Batalha.hs
+++ Batalha.hs
@@ -14,10 +14,14 @@ selecionaAtributoRelacionado Carisma personagem = carisma personagem
 turnoConsumivel :: Personagem -> Personagem -> Consumivel -> Personagem 
 turnoConsumivel personagemEmissor personagemReceptor consumivel = usarItemConsumivel consumivel personagemReceptor 
 turnoHabilidade :: Personagem -> Personagem -> Habilidade -> Int -> Int-> Personagem
-turnoHabilidade personagemEmissor personagemReceptor habilidade numeroDado1 numeroDado2 = if (isImune personagemReceptor (tipoDeDano habilidade) && (selecionaAtributoRelacionado (atributo_relacionado habilidade) personagemEmissor) + (min numeroDado1 numeroDado2) >= (pontosParaAcerto (habilidade))) then usaHabilidade habilidade personagemReceptor
+turnoHabilidade personagemEmissor personagemReceptor habilidade numeroDado1 numeroDado2 = if (temImunidade personagemReceptor (tipoDeDano habilidade) && (selecionaAtributoRelacionado (atributo_relacionado habilidade) personagemEmissor) + (min numeroDado1 numeroDado2) >= (pontosParaAcerto (habilidade))) then usaHabilidade habilidade personagemReceptor
                                                                                 else if (selecionaAtributoRelacionado (atributo_relacionado habilidade) personagemEmissor + (numeroDado1 + numeroDado2) >= pontosParaAcerto habilidade) then usaHabilidade habilidade personagemReceptor
                                                                                 else personagemReceptor    
 
                                                                                 
-isImune :: Personagem -> TipoDano -> Bool
-isImune personagem habilidade = habilidade `elem` (imunidades personagem)
+taVivo:: Personagem -> Bool
+taVivo personagem = (vida personagem) > 0
+
+temImunidade :: Personagem -> TipoDano -> Bool
+temImunidade personagem habilidade = habilidade `elem` (imunidades personagem)
+
