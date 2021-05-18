4ef6bf7101d35c5b689a5d99b05b5514fd8e1e9b
Rodrigo Torres Meira <rodrigotmeira@hotmail.com>
2021-05-18 20:34:54 -0300

    Inicio do main, printando as opções

---
+++ Prolog/Main.pl
@@ -0,0 +1,36 @@
+main :- 
+    menu('start'),
+    halt.
+
+
+menu('start') :-
+    criaArquivos,
+    writeln('                  ______________'),
+    writeln('            ,===:\'.,            `-._'),
+    writeln('                 `:.`---.__         `-._'),
+    writeln('                   `:.     `--.         `.'),
+    writeln('                     \\         `.         `.'),
+    writeln('             (,,(,    \\          `.   ____,-`.,'),
+    writeln('          (,\'     `/   \\    ,--.___`.\''),
+    writeln('       ( ,\'   ,--.  `,  \\ ;\'         `'),
+    writeln('      {,O {      \\   :   \\;'),
+    writeln('        \\  ,,\'    /  /    //'),
+    writeln('         |;;    /  ,\' ,-//.    ,---.      ,'),
+    writeln('        \\;\'   /  ,\' /  _  \\   /  _  \\    ,\'/'),
+    writeln('             \\    `\'  / \\   `\'  / \\   `.\' /'),
+    writeln('                `.___,\'   `.__,\'   `.__,\'  VZ '),
+    writeln('1 - Ler Campanha\n2 - Definir Lore da campanha\n3 - Menu de Personagem\n4 - Menu de Item\n5 - Menu de Habilidades\n6 - Loja\n9 - Sair\n'),
+    read(Entrada),
+    menu(Entrada).
+
+menu(X) :-
+    write('A entrada: '),
+    write(X),
+    writeln('. Nao eh valida.'),
+    write('vlw flw. ps eu te amo').
+
+
+criaArquivos :-
+    writeln('Falta implementar criacao de arquivos'),
+    read(_).
+    
