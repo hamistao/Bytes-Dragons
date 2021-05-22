372f9f69c07e68e09062ebeff91bdbb3edec2b5c
MelquiSilva <melquisedeque.silva@ccc.ufcg.edu.br>
2021-05-22 18:23:49 -0300

    Adicionando criacao de itens

---
+++ Prolog/Item.pl
@@ -0,0 +1,10 @@
+construtorItemConsumivel(Nome, AlteracaoVida, AlteracaoVelocidadeConsumivel, Duracao, consumivel(Nome, AlteracaoVida, AlteracaoVelocidadeConsumivel, Duracao)).
+
+construtorItemEquipavel(NomeEquipavel, AlteracaoVidaMaxima, AlteracaoForca,AlteracaoInteligencia, AlteracaoSabedoria, AlteracaoConstituicao, AlteracaoCarisma, AlteracaoVelocidadeEquipavel, TipoEquipavel, equipavel(NomeEquipavel, AlteracaoVidaMaxima, AlteracaoForca, AlteracaoInteligencia, AlteracaoSabedoria, AlteracaoConstituicao, AlteracaoCarisma, AlteracaoVelocidadeEquipavel, TipoEquipavel)).
+
+isTipoEquipavel("Cabeca").
+isTipoEquipavel("Torso").
+isTipoEquipavel("Pernas").
+isTipoEquipavel("Maos").
+isTipoEquipavel("Arma").
+
