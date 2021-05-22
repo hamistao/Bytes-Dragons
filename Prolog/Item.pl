af021b0078d5c58bce5931d23766ca142df1d9b5
Rodrigo Torres Meira <rodrigotmeira@hotmail.com>
2021-05-22 18:32:54 -0300

    Colocando destreza

--- Prolog/Item.pl
+++ Prolog/Item.pl
@@ -1,6 +1,6 @@
 construtorItemConsumivel(Nome, AlteracaoVida, AlteracaoVelocidadeConsumivel, Duracao, consumivel(Nome, AlteracaoVida, AlteracaoVelocidadeConsumivel, Duracao)).
 
-construtorItemEquipavel(NomeEquipavel, AlteracaoVidaMaxima, AlteracaoForca,AlteracaoInteligencia, AlteracaoSabedoria, AlteracaoConstituicao, AlteracaoCarisma, AlteracaoVelocidadeEquipavel, TipoEquipavel, equipavel(NomeEquipavel, AlteracaoVidaMaxima, AlteracaoForca, AlteracaoInteligencia, AlteracaoSabedoria, AlteracaoConstituicao, AlteracaoCarisma, AlteracaoVelocidadeEquipavel, TipoEquipavel)).
+construtorItemEquipavel(NomeEquipavel, AlteracaoVidaMaxima, AlteracaoForca, AlteracaoInteligencia, AlteracaoSabedoria, AlteracaoDestreza, AlteracaoConstituicao, AlteracaoCarisma, AlteracaoVelocidadeEquipavel, TipoEquipavel, equipavel(NomeEquipavel, AlteracaoVidaMaxima, AlteracaoForca, AlteracaoInteligencia, AlteracaoSabedoria, AlteracaoDestreza, AlteracaoConstituicao, AlteracaoCarisma, AlteracaoVelocidadeEquipavel, TipoEquipavel)).
 
 isTipoEquipavel("Cabeca").
 isTipoEquipavel("Torso").
