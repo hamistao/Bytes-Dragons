079f6297b9c76f0bea6ba05cec59a3f156fface3
Pedro Ribeiro <pedro.antonio.ribeiro@ccc.ufcg.edu.br>
2021-05-22 22:02:36 -0300

    adicionando arquivo referente aos predicados de batalha.

---
+++ Prolog/Batalha.pl
@@ -0,0 +1,13 @@
+selecionaAtributoRelacionado("Forca", Personagem(Forca, Inteligencia, Sabedoria, Destreza, Constituicao, Carisma), Valor) :-
+    Valor is Forca.
+selecionaAtributoRelacionado("Inteligencia", Personagem(Forca, Inteligencia, Sabedoria, Destreza, Constituicao, Carisma), Valor) :-
+    Valor is Inteligencia.
+selecionaAtributoRelacionado("Sabedoria", Personagem(Forca, Inteligencia, Sabedoria, Destreza, Constituicao, Carisma), Valor) :-
+    Valor is Sabedoria.
+selecionaAtributoRelacionado("Destreza", Personagem(Forca, Inteligencia, Sabedoria, Destreza, Constituicao, Carisma), Valor) :-
+    Valor is Destreza.
+selecionaAtributoRelacionado("Constituicao", Personagem(Forca, Inteligencia, Sabedoria, Destreza, Constituicao, Carisma), Valor) :-
+    Valor is Constituicao.
+selecionaAtributoRelacionado("Carisma", Personagem(Forca, Inteligencia, Sabedoria, Destreza, Constituicao, Carisma), Valor) :-
+    Valor is Carisma.
+
