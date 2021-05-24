d56fc88f00473519127626e405751a10d4c54cb5
Rodrigo <rodrigotmeira@hotmail.com>
2021-05-24 20:25:00 -0300

    Personagem com coisas novas

--- Prolog/MainPersonagem.pl
+++ Prolog/MainPersonagem.pl
@@ -28,86 +28,61 @@ menuPersg :-
 
 
 menuPersg("1") :-
-    structsFromFile('data/persngs.bd', Personagens),
-    listaPersonagens(Personagens, PersonagensNomes),
     writeln('Personagens: '),
-    stringFromList(PersonagensNomes, PersonagensStr),
-    writeln(PersonagensStr),
-    readEntrada(_),!,
+    listarPersonagens,
+    readEntrada(_),
     menuPersg.
 
 menuPersg("2") :-
     nl, writeln('Qual o Nome do Personagem?'),
     readEntrada(Nome),
+    personagemExiste(Nome),
     writeln('Qual a Classe do Personagem?'),
     readEntrada(Classe),
     checkClasse(Classe),
     writeln('Qual a Raca do Personagem?'),
     readEntrada(Raca),
     checkRaca(Raca),
-    deleteWithName(Nome),
-    open('data/persngs.bd', append, Str),
-    construtorPersonagem(Nome, Raca, Classe, Personagem),
-    writeln(Personagem),
-    write(Str, Personagem), writeln(Str, "."),!,
-    close(Str),
+    retract_personagem(Nome, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _),
+    assert_personagem(Nome, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _),
     menuPersg.
 
 menuPersg("3") :-
     writeln('Qual o Nome do Personagem desejado?'),
     readEntrada(Nome),
-    structsFromFile('data/persngs.bd', Personagens),
-    exibeByName(Personagens, Nome),!,
+    exibePersonagem(Nome),
     readEntrada(_), menuPersg.
-
 
 menuPersg("4") :-
     writeln('Qual o Nome do Personagem a ser deletado?'),
     readEntrada(Nome),
-    deleteWithName(Nome), menuPersg.
+    personagemExiste(Nome),
+    retract_personagem(Nome, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _),
+    write('Personagem deletado'),
+    readEntrada(_), menuPersg.
+
+menuPersg("5") :-
+    menuItemPersonagem,
+    menuPersg.
+
+menuPersg("6") :-
+    writeln('Qual a Resistencia? (Cortante | Magico | Venenoso | Fogo | Gelo | Fisico)'),
+    readEntrada(Tipo),
+    writeln('Qual o Nome do Personagem?'),
+    readEntrada(Nome),
+    personagemExiste(Nome),
+    assert_resistencia(Nome, Tipo).
+
+
+menuPersg("7") :-
+    menuBatalha,
+    menuPersg.
 
 
 menuPersg("9").
 
 menuPersg(_):-
     menuPersg.
-
-
-
-exibeByName([], Nome) :-
-    string_concat('\nNome: ', Nome, S1),
-    string_concat(S1, "\nNao eh valido.", S2),
-    writeln(S2).
-
-exibeByName([Personagem|L], Nome):-
-    nomePersonagem(Personagem, Nome),
-    exibePersonagem(Personagem, S),
-    writeln(S).
-
-exibeByName([X|L], Nome) :-
-    exibeByName(L, Nome).
-
-
-
-deleteWithName(Nome) :- 
-    structsFromFile('data/persngs.bd', Personagens),
-    deleteByName(Personagens, Nome, PersonagensFinais),
-    open('data/persngs.bd', write, Str),
-    writeLinesToFile(Str, PersonagensFinais), !,
-    close(Str).
-
-
-deleteByName([], _, []).
-deleteByName([X|L], Nome, L) :-
-    nomePersonagem(X, NomeTeste),
-    Nome == NomeTeste.
-
-deleteByName([X|L], Nome, R) :-
-    deleteByName(L, Nome, R1).
-    append([X], R1, R).
-
-
-
 
 
 checkRaca(Raca):-
@@ -124,3 +99,137 @@ checkClasse(C) :-
     string_concat(C, ' nao eh uma classe valida', S),
     writeln(S),!,
     false.
+
+
+menuItemPersonagem :-
+    write('\e[H\e[2J'),
+    writeln(  '  ,   A           {}'),
+    writeln(  ' / \\, | ,        .--.'),
+    writeln(  '|    =|= >      /.--.\\ '),
+    writeln(  ' \\ /` | `       |====|'),
+    writeln(  '  `   |         |`::`| ' ),
+    writeln(  '      |     .-;`\\..../`;_.-^-._'),
+    writeln(  '     /\\ \\/ /  |...::..|`   :   `|'),
+    writeln(  '     |:\'\\ |   /\'\'\'::\'\'|   .:.   |'),
+    writeln(  '      \\ /\\;-,/\\.  ::  |..:::::..|'),
+    writeln(  '      |\\ <` >  >._::_.| \':::::\' |'),
+    writeln(  '      | `""`  /   ^^  |   \':\'   |'),
+    writeln(  '      |       |       \\    :    /'),
+    writeln(  '      |       |        \\   :   /' ),
+    writeln(  '      |       |___/\\___|`-.:.-`'),
+    writeln(  '      |        \\_ || _/    `'),
+    writeln(  '      |        <_ >< _>'),
+    writeln(  '      |        |  ||  |'),
+    writeln(  '      |        |  ||  |'),
+    writeln(  '      |       _\\.:||:./_'),
+    writeln(  '      | jgs  /____/\\____\\'),
+    writeln(  '1 - Equipar Item\n2 - Equipar Consumivel\n3 - Alocar Habilidade\n4 - Desequipar Item\n5 - Desequipar Consumivel\n6 - Desalocar Habilidade\n9 - Voltar Menu\n'),
+    readEntrada(Entrada),
+    menuItemPersonagem(Entrada).
+
+menuItemPersonagem("1"):-
+    nl, writeln('Qual o nome do Personagem?'),
+    readEntrada(Nome),
+    personagemExiste(Nome),
+    writeln('Qual o nome do Equipavel?'),
+    readEntrada(Equipavel),
+    equipavelExiste(Equipavel),
+    colocaItemPersonagem(Nome, Equipavel).
+
+menuItemPersonagem("2") :-
+    nl, writeln('Qual o nome do Personagem?'),
+    readEntrada(Nome),
+    personagemExiste(Nome),
+    writeln('Qual o nome do Consumivel?'),
+    readEntrada(Consumivel),
+    consumivelExiste(Consumivel),
+    colocaConsumivelPersonagem(Nome, Consumivel).
+
+menuItemPersonagem("3") :-
+    nl, writeln('Qual o nome do Personagem?'),
+    readEntrada(Nome),
+    personagemExiste(Nome),
+    writeln('Qual o nome da Habilidade?'),
+    readEntrada(Habilidade),
+    habilidadeExiste(Habilidade),
+    colocaHabilidadePersonagem(Nome, Habilidade).
+
+menuItemPersonagem("4") :-
+    nl, writeln('Qual o nome do Personagem?'),
+    readEntrada(Nome),
+    personagemExiste(Nome),
+    writeln('Qual o nome da Equipavel?'),
+    readEntrada(Equipavel),
+    equipavelExiste(Equipavel),
+    tiraEquipavelPersonagem(Nome, Equipavel).
+
+menuItemPersonagem("5") :-
+    nl, writeln('Qual o nome do Personagem?'),
+    readEntrada(Nome),
+    personagemExiste(Nome),
+    writeln('Qual o nome da Consumivel?'),
+    readEntrada(Consumivel),
+    consumivelExiste(Consumivel),
+    tiraConsumivelPersonagem(Nome, Consumivel).
+
+menuItemPersonagem("6") :-
+    nl, writeln('Qual o nome do Personagem?'),
+    readEntrada(Nome),
+    personagemExiste(Nome),
+    writeln('Qual o nome da Habilidade?'),
+    readEntrada(Habilidade),
+    habilidadeExiste(Habilidade),
+    tiraHabilidadePersonagem(Nome, Habilidade).
+
+menuItemPersonagem("9").
+
+menuItemPersonagem(_):-
+    write('\nEntrada invalida.'),
+    menuItemPersonagem.
+
+
+colocaHabilidadePersonagem(Nome, Habilidade) :-
+    retract_personagemTemHabilidade(Nome, Habilidade),
+    assert_personagemTemHabilidade(Nome, Habilidade).
+
+colocaHabilidadePersonagem(Nome, Habilidade) :-
+    assert_personagemTemHabilidade(Nome, Habilidade).
+
+
+colocaConsumivelPersonagem(Nome, Consumivel) :-
+    retract_personagemTemConsumivel(Nome, Consumivel),
+    assert_personagemTemConsumivel(Nome, Consumivel).
+
+colocaConsumivelPersonagem(Nome, Consumivel) :-
+    assert_personagemTemConsumivel(Nome, Consumivel).
+
+colocaEquipavelPersonagem(Nome, Equipavel) :-
+    retract_personagemTemEquipavel(Nome, Equipavel),
+    assert_personagemTemEquipavel(Nome, Equipavel).
+
+colocaEquipavelPersonagem(Nome, Equipavel) :-
+    assert_personagemTemEquipavel(Nome, Equipavel).
+
+tiraEquipavelPersonagem(Nome, Equipavel).
+    retract_personagemTemEquipavel(Nome, Equipavel).
+tiraEquipavelPersonagem(_, _).
+
+tiraConsumivelPersonagem(Nome, Consumivel).
+    retract_personagemTemConsumivel(Nome, Consumivel).
+tiraConsumivelPersonagem(_, _).
+
+tiraHabilidadePersonagem(Nome, Habilidade).
+    retract_personagemTemHabilidade(Nome, Habilidade).
+tiraHabilidadePersonagem(_, _).
+
+personagemExiste(Nome) :-
+    personagem(Nome, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _).
+
+consumivelExiste(Nome) :-
+    consumivel(Nome, _, _, _).
+
+habilidadeExiste(Nome) :-
+    habilidade(Nome, _, _, _, _, _).
+
+equipavelExiste(Nome) :-
+    equipavel(Nome, _, _, _, _, _, _, _, _, _).
