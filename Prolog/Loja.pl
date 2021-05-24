da0dcfc2193ff83a00502896c0a0a01a03626c47
Juhrer <lucas.vigolvino@ccc.ufcg.edu.br>
2021-05-24 14:28:05 -0300

    Arrumei o exibir em loja

--- Prolog/Loja.pl
+++ Prolog/Loja.pl
@@ -6,15 +6,22 @@ construtorLoja(Nome, R) :-
     string_concat(S4, '[[]]', S5),
     string_concat(S5, ')', R).
 
-exibir(loja(Nome, [])) :-
+exibir(loja(Nome, Equipaveis, Consumiveis), R) :-
     string_concat("\nNome: ", Nome, S1),
-    string_concat("\nItens a venda:\nEquipaveis:\n"),
+    string_concat(S1, "\nItens a venda:\nEquipaveis:\n", S2),
+    catalogo(Equipaveis, S3),
+    stringFromList(S3, S4),
+    string_concat(S2, S4, S5),
+    string_concat(S5, "\nConsumiveis:\n", S6),
+    catalogo(Consumiveis, S7),
+    stringFromList(S7, S8),
+    string_concat(S6, S8, R).
 
 catalogo([], []).
 catalogo([[Item, Preco] | L], R) :-
     catalogo(L, R1),
     nomeItem(Item, Nome),
-    string_concat("\nNome: ", Nome, S1),
+    string_concat("Nome: ", Nome, S1),
     string_concat(S1, "\nPreco: ", S2),
     string_concat(S2, Preco, S3),
     string_concat(S3, "\n", S4).
